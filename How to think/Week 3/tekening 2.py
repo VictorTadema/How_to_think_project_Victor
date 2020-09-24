7fa5538bce264a50799bb439f5cc7f925042d2f4
Victor <vcj.tadema@gmail.com>
2020-09-24 19:18:04 +0200

    homework for week 3

---
+++ How to think/Week 3/tekening 2.py
@@ -0,0 +1,18 @@
+import turtle
+
+paper = turtle.Screen()
+leonard = turtle.Turtle()
+
+leonardo.foward(50)
+
+leonardo.stamp()
+leonardo.penup()
+leonardo.foward(50)
+leonardo.pendown()
+leonardo.stamp()
+
+leonardo.forward(50)
+leonardo.left(60)
+leonardo.forward(50)
+
+paper.exitonclick()
---
+++ How to think/Week 3/tiemen creatie.py
@@ -0,0 +1,15 @@
+import turtle
+
+paper = turtle.Screen()
+Tiemen = turtle.Turtle()
+
+window = turtle.Screen()
+Tiemen = turtle.Turtle()
+Tiemen.shape("circle")
+Tiemen.forward(130)
+Tiemen.left(90)
+Tiemen.forward(130)
+Tiemen.left(90)
+Tiemen.forward(130)
+Tiemen.left(90)
+Tiemen.forward(130)
