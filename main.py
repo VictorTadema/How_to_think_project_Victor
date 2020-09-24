e8db9f2f1527120324c1b26445858c92d569bf49
Victor <vcj.tadema@gmail.com>
2020-09-24 16:55:23 +0200

    homework for week 2

---
+++ main.py
@@ -0,0 +1,28 @@
+a = "All"
+b = " work"
+c = " and"
+d = " no"
+e = " play"
+f = " makes"
+g = " Jack"
+h = " a"
+i = " dull"
+j = " boy"
+
+print (a+b+c+d+e+f+g+h+i+j)
+
+print(6 *(1 -2))
+
+# ah lekker bier
+
+bruce = 6
+print(bruce + 4)
+
+x = str(51 % 24 +2)
+y = " uur"
+print(x+y)
+
+
+response = input(100) #principal amount
+
+
