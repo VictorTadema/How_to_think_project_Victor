9f24440a4d8e8cd7377b1e7536af7a5c6464eba6
Victor <vcj.tadema@gmail.com>
2020-09-27 15:10:03 +0200

    homework for week 3

---
+++ How to think/Week 3/Exercise 14.py
@@ -0,0 +1,17 @@
+for numbers in [12, 10, 32, 3, 66, 17, 42, 99, 20]:
+    line = numbers
+    print(numbers)
+
+for numbers in [12, 10, 32, 3, 66, 17, 42, 99, 20]:
+    squares = numbers**2
+    print(numbers, squares)
+
+total = 0
+for numbers in [12, 10, 32, 3, 66, 17, 42, 99, 20]:
+    total += numbers
+print(total)
+
+tot = 1
+for numbers in [12, 10, 32, 3, 66, 17, 42, 99, 20]:
+    tot *= numbers
+print(tot)
