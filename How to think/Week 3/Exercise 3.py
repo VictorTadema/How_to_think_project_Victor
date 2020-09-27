60bef2e9f0de0a5084ca4b0335601f142313ee20
Victor <vcj.tadema@gmail.com>
2020-09-27 12:42:18 +0200

    homework for week 3

---
+++ How to think/Week 3/Exercise 3.py
@@ -0,0 +1,13 @@
+# 1. a > b
+#    a <= b
+
+# 2. a >= b
+#    a < b
+
+# 3. a >= 18 and day == 3
+#    a < 18 and day != 3
+
+# 4. a >= 18 and day != 3
+#    a < 18 or day == 3
+
+
---
+++ How to think/Week 3/Exercise 4.py
@@ -0,0 +1,11 @@
+# 1. 3 == 3
+#    3 is equal to 3, which is true
+
+# 2. 3 != 3
+#    3 is not equal to 3, which is false
+
+# 3. 3 >= 4
+#    3 is either equal to 4 or higher than 4, which is false
+
+# 4. not (3 < 4)
+#    3 is not smaller than 4, so 3 is bigger than 4, which is false
